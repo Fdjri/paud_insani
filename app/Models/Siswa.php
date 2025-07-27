f966887d73ef546fd6ec7641d06b23de8f4579f2
Sholihul Fadjri Triwibowo <88471883+Fdjri@users.noreply.github.com>
2025-07-27 20:07:05 +0700

    update model siswa

--- app/Models/Siswa.php
+++ app/Models/Siswa.php
@@ -6,12 +6,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Siswa extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
-    protected $table = 'siswas';
+    protected $table = 'siswas'; 
+
     protected $guarded = [];
 
     public function kelas(): BelongsTo
